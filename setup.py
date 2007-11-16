e416e10bb68d906c509f77b571f49a1ae7c9e4b7
dencheva <dencheva@stsci.edu>
2007-11-16 21:12:19 +0000

    Changed setup.py to install a package
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/wfpc2tools@1001 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(name = "wfpc2tools",
       author_email = "help@stsci.edu",
       license = "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
       platforms = ["Linux","Solaris","Mac OS X","Win"],
-      py_modules = ['wfp2cte','wfpc2destreak'],
-      package_dir={'':'lib'},
+      packages = ['wfpc2tools'],
+      package_dir={'wfpc2tools':'lib'},
       )
 
 
