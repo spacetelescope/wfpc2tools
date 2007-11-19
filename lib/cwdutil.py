da64adc43be1eb1c86411ef88441432d25891f50
grumm <grumm@stsci.edu>
2007-11-19 20:31:45 +0000

    utility functions
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/wfpc2tools@1011 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ lib/cwdutil.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+import sys
+import time
+
+# Utility functions and parameters for CalWFPC2Destreak
+
+QUIET = 0 # verbosity levels
+VERBOSE = 1
+VERY_VERBOSE = 2   
+                                                                                
+# default value
+verbosity = VERBOSE
+
+def all_printMsg( message, level=VERBOSE):
+
+    if verbosity >= level:     
+      print message
+      sys.stdout.flush()
+
+def printMsg( message, level=QUIET):
+
+    if verbosity >= level:
+        print message
+        sys.stdout.flush()
+
+def setVerbosity( verbosity_level):
+    """Copy verbosity to a variable that is global for this file.                                                              
+       argument: verbosity_level -  an integer value indicating the level of verbosity
+    """
+                                                                                
+    global verbosity
+    verbosity = verbosity_level
+
+def checkVerbosity( level):
+    """Return true if verbosity is at least as great as level."""
+
+    return (verbosity >= level)
+
+
