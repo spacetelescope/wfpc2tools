cbffb8316d6644424b5cf640bf0fcb79e033d465
laidler <laidler@stsci.edu>
2008-02-15 21:04:09 +0000

    Adding some documentation to the top level of the wfpc2tools package
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/wfpc2tools@1149 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,2 +1,16 @@
+"""The wfpc2tools package holds Python tasks useful for analyzing WFPC2 data.
+
+These tasks include:
+      WFPC2CTE - computes the CTE degradation & updates header keywords
+
+      
+This release also includes alpha versions of the following tasks:
+     Wfpc2destreak - Calculate magnitude of, and remove, streaks from data
+
+Utility and library functions used by these tasks are also included in this
+module.
+
+
+"""
 import wfpc2cte
 import wfpc2destreak
