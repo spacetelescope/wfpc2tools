d0095e9760523f63b4bf8507cec9e8345dcdbd49
sienkiew <sienkiew@stsci.edu>
2008-08-13 21:11:10 +0000

    setup.py mods - converting more packages
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/wfpc2tools@6921 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -1,30 +1,4 @@
-from distutils.core import setup
-import sys, os.path
+#!/usr/bin/env python
 
-if not hasattr(sys, 'version_info') or sys.version_info < (2,3,0,'alpha',0):
-    raise SystemExit, "Python 2.3 or later required to build stistools."
-
-args = sys.argv[:]
-
-for a in args:
-    if a.startswith('--local='):
-        dir = os.path.abspath(a.split("=")[1])
-        sys.argv.extend([
-                "--install-lib="+dir,
-                "--install-scripts=%s" % dir])
-        args.remove(a)
-        sys.argv.remove(a)
-
-setup(name = "wfpc2tools",
-      version = "1.0.0",
-      description = "Python Tools for WFPC2 Data",
-      author = "Warren Hack, Nadezhda Dencheva, David Grumm",
-      author_email = "help@stsci.edu",
-      license = "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
-      platforms = ["Linux","Solaris","Mac OS X","Win"],
-      packages = ['wfpc2tools'],
-      package_dir={'wfpc2tools':'lib'},
-      )
-
-
-
+import pytools.stsci_distutils_hack
+pytools.stsci_distutils_hack.run(pytools_version = "3.0")
