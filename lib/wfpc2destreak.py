29f9dbc901bd448f8beee63fbdc3279b222f81ff
grumm <grumm@stsci.edu>
2008-10-02 19:19:46 +0000

    add check for name = wfpc2tools.wfpc2destreak
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/wfpc2tools@7120 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/wfpc2destreak.py
+++ lib/wfpc2destreak.py
@@ -181,7 +181,7 @@ class Wfpc2destreak:
 
         # do some parameter type checking
         print ' start of init __name__ = ',__name__   # 093009
-        if (( __name__ == 'wfpc2destreak') | ( __name__ == 'wdestreak')):  # for python interface, set defaults and check unspecified pars
+        if (( __name__ == 'wfpc2destreak') | ( __name__ == 'wdestreak') | ( __name__ == 'wfpc2tools.wfpc2destreak')):  # for python interface, set defaults and check unspecified pars
            [group, bias_thresh, row_thresh, niter] = check_py_pars(input_file, group, bias_thresh, row_thresh, \
                                                                                     input_mask, niter)
         else:
