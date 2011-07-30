615de3bafeb28dcf2fffc1d9c9cb45a2c6e3a9a2
sienkiew <sienkiew@stsci.edu>
2011-07-30 22:56:17 +0000

    temporarily revert to the old install system
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/wfpc2tools@13425 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ defsetup.py
@@ -0,0 +1,14 @@
+from __future__ import division # confidence high
+
+pkg =  "wfpc2tools"
+
+setupargs = {
+    'version' : 		"1.0.0",
+    'description' :	    "Python Tools for WFPC2 Data",
+    'author' : 		    "Warren Hack, Nadezhda Dencheva, David Grumm",
+    'author_email' : 	"help@stsci.edu",
+    'license' : 		"http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
+    'platforms' : 	    ["Linux","Solaris","Mac OS X","Win"],
+    'package_dir' :     { 'wfpc2tools' : 'lib/wfpc2tools' },
+}
+
